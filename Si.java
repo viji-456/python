2e2356c4f90b92f6d8129a5e21b936026d99d07b
viji-456 <152067769+viji-456@users.noreply.github.com>
2024-10-05 09:28:30 +0530

    Add files via upload

---
+++ Si.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Si {
+
+    public static void main(String[] args){
+        
+        Scanner  sc =new Scanner(System.in);
+            int p=sc.nextInt();
+            int t=sc.nextInt();
+            double r=sc.nextDouble();
+            double var=p*t*r/100;
+            System.out.println(var);
+         }
+        } 
+
