7e39a01297cacc9e32be1aa42c5603d43ffb8e9e
viji-456 <152067769+viji-456@users.noreply.github.com>
2024-10-05 09:27:22 +0530

    Add files via upload

---
+++ swap.java
@@ -0,0 +1,10 @@
+public class swap {
+    public static void main(String[] args){
+        int temp,a=5,b=19;
+        temp =a;
+        a=b;
+        b=temp;
+        System.out.println(temp);
+        }
+    }
+
